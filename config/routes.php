4f14abc563ee448bf8c34b5dc70f17f0da49454b
Sara Sanchez <saralesan@hotmail.com>
2020-05-15 00:26:13 -0500

    admin route prefix

--- config/routes.php
+++ config/routes.php
@@ -85,6 +85,13 @@ $routes->scope('/', function (RouteBuilder $builder) {
     $builder->fallbacks();
 });
 
+$routes->prefix('admin', function (RouteBuilder $routes) {
+    // All routes here will be prefixed with `/admin`, and
+    // have the `'prefix' => 'Admin'` route element added that
+    // will be required when generating URLs for these routes
+    $routes->fallbacks(DashedRoute::class);
+});
+
 /*
  * If you need a different set of middleware or none at all,
  * open new scope and define routes there.
