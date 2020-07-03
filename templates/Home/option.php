30a849a48e0d9acbfc731ee7d30ad10c82da50d2
Sara Sanchez <saralesan@hotmail.com>
2020-07-03 00:48:15 -0500

    option page update

--- templates/Home/option.php
+++ templates/Home/option.php
@@ -1,16 +1,23 @@
 <div id="branches-options">
-    <h2 class="center-align"><?= __('Choose a branch') ?></h2>
-    <div class="container">
+    <p class="center-align"><?= __('Choose a branch') ?></p>
+    <div class="item-wrap">
         <?php foreach ($branches as $branch) : ?>
-        <div class="option-item">
-            <?php 
-            $imagePath = str_replace(WWW_ROOT, '', $branch->image_dir) . DS;
-            echo $this->Html->image($branch->image , [
-                "alt" => "logo",
-                'url' => ['controller' => 'Home', 'action' => 'option', $branch->id],
-                'pathPrefix' => $imagePath
-                ]);
-            ?>
+        <div class="item">
+            <a href="/home/option/<?= $branch->id ?>">
+                <div>
+                    <?php 
+                    $imagePath = str_replace(WWW_ROOT, '', $branch->image_dir) . DS;
+                    $imageURL = str_replace('\\', '/', $imagePath);
+                    ?>
+                    <div class="branch-image"
+                        style="background: url('<?= $imageURL . $branch->image ?>') center center; background-size: cover;">
+                    </div>
+
+                    <div>
+                        <h2 class="center-align white-text">Danalvial Sede <?= $branch->name ?></h2>
+                    </div>
+                </div>
+            </a>
         </div>
         <?php endforeach; ?>
     </div>
